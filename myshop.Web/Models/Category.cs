5f16a2b454b6604ceb452294ff00a323d173caf9
Ahamdy11 <ahamdy01@yahoo.com>
2024-08-05 05:02:42 +0200

    Category Migration

---
+++ myshop.Web/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace myshop.Web.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]// Name is Not NULL
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedTime { get; set; } = DateTime.Now;
+    }
+}
--- myshop.Web/obj/Debug/net8.0/myshop.Web.AssemblyInfo.cs
+++ myshop.Web/obj/Debug/net8.0/myshop.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myshop.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b747b828037a501fbf69fb6f765ccb5f8e8f4a1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b08dbfc27e8d54da5056732a0c92113f433c99a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("myshop.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myshop.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
